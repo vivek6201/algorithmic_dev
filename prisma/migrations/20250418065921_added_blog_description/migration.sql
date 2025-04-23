26cf727e9f311ee298017c43c426836917a08a98
vivek6201 <gvicky4567888@gmail.com>
2025-04-23 14:08:33 +0530

    feat: remove obsolete migration files and clean up database schema

--- prisma/migrations/20250418065921_added_blog_description/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `description` to the `blogs` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "blogs" ADD COLUMN     "description" TEXT NOT NULL;
