24800463eb1a01804478aafb50aa418bfd20faa5
vivek6201 <gvicky4567888@gmail.com>
2025-04-28 16:13:05 +0530

    feat:added prettier

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
-        hostname: "uploads.sitepoint.com",
+        hostname: 'uploads.sitepoint.com',
       },
     ],
   },
   swcMinify: true,
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000", "algorithmicdev.in"],
+      allowedOrigins: ['localhost:3000', 'algorithmicdev.in'],
     },
   },
 };
