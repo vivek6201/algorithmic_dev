5f13361a392dc8e099a741d682d18528e1683485
vivek6201 <gvicky4567888@gmail.com>
2025-06-29 16:51:43 +0530

    add code highlight

--- packages/ui/src/components/elements/CodeBlock.tsx
+++ packages/ui/src/components/elements/CodeBlock.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { Copy, Check } from '@repo/ui';
 import { cn } from '@repo/ui/lib/utils';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 type CodeBlockProps = {
   code: string;
@@ -20,7 +22,7 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ code }) => {
 
   return (
     <div className="not-prose bg-[#1e1e1e] rounded-lg overflow-hidden shadow-md my-4">
-      <div className="border-b bg-gray-400 dark:bg-gray-900 py-2 px-5 flex justify-between items-center">
+      <div className="border-b bg-[#1D1F21] dark:bg-gray-900 py-2 px-5 flex justify-between items-center shadow-md dark:shadow-none border-none">
         <div className="flex gap-2 items-center">
           {colors.map((classname) => (
             <div key={classname} className={cn('rounded-full w-2.5 aspect-square', classname)} />
@@ -35,9 +37,9 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ code }) => {
         </button>
       </div>
 
-      <pre className="p-4 overflow-x-auto text-sm text-gray-100 bg-gray-800 dark:bg-transparent">
-        <code>{code}</code>
-      </pre>
+      <SyntaxHighlighter language="javascript" style={atomDark}>
+        {code}
+      </SyntaxHighlighter>
     </div>
   );
 };
